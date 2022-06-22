0c3d7651a3e98bb1094a2fb36cedb1da0e59c7d7
Fang stålmarck <fanghs@kth.se>
2022-06-22 11:37:26 +0200

    unit tests

--- UnitTests/CarsSpecsBrandIsPlanborgini.cs
+++ UnitTests/CarsSpecsBrandIsPlanborgini.cs
@@ -7,7 +7,7 @@ using System.Drawing;
 using static CarFactory.Controllers.CarController;
 using CarFactory.Controllers;
 
-//To test Carcontroller's POST method , Post Json body are:
+//To test Carcontroller's POST method , Post Json body is:
 //        {
 //  "cars": [
 //    {
@@ -30,14 +30,14 @@ using CarFactory.Controllers;
 //    }
 //  ]
 //}
-// |Planborgini|3 doors|Pink base with red dots|10 subwoofers and 20 standard|15| if it is a Planborgini than 3 doors, pink base and red sdots
+// |PlanfaRomeo|3 doors|Pink base with red dots|10 subwoofers and 20 standard|15| if it is a PlanfaRomeo than 3 doors, pink base and red sdots
 
 namespace UnitTests
 {
     [TestClass]
-    public class CarsSpecsBrandIsPlanborgini
+    public class CarsSpecsBrandIsPlanfaRomeo
     {
-        //Arange, CarsSpecsBrand_Planborgini
+        //Arange, CarsSpecsBrand_PlanfaRomeo
         public static CarPaintSpecificationInputModel spec = new CarPaintSpecificationInputModel
         {
             Type = "Stripe ",
@@ -65,58 +65,53 @@ namespace UnitTests
         //};
 
         [TestMethod]
-        public void PaintType_Stripe_With_Capital_Letter_Should_Work_As_Small_lettter()
+        public void PaintType_Stripe_With_Capital_Letter_Should_Work_As_Small_lettter_Stripe()
         {
-            //var paintType = new CarPaintSpecificationInputModel().Type;
             var paintType = spec.Type;
             var painterJob = new StripedPaintJob(ParseColor(spec.BaseColor), ParseColor(spec.StripeColor));
             var painter = new Painter();
-            var car = new Car(new Chassis("", true), new Engine(new EngineBlock(10), "Test"), new Interior(), new Wheel[4], Manufacturer.Plandrover, 5);
+            var car = new Car(new Chassis("", true), new Engine(new EngineBlock(10), "Test"), new Interior(), new Wheel[4], Manufacturer.PlanfaRomeo, 5);
             painter.PaintCar(car, painterJob);
             var job = (StripedPaintJob)car.PaintJob;
-            job.StripeColor.Should().Be(Color.Orange);
-        }
-
-        public void CarsSpecsBrand_Planborgini_Should_Have_Quantity_15()
-        {
-            CarsSpecsBrand_Planborgini_Should_Have_Quantity_15();
+            job.Should().NotBeNull();
         }
 
         [TestMethod]
-        public void CarsSpecsBrand_Planborgini_Should_Have_Quantity_15(CarController carController)
+        public void CarsSpecsBrand_PlanfaRomeo_Should_Have_Quantity_75()
         {
 
         }
 
         [TestMethod]
-        public void CarsSpecsBrand_Planborgini_Should_Have_Three_Doors()
+        public void CarsSpecsBrand_PlanfaRomeo_Should_Have_Three_Doors()
         {
          
         }
 
         [TestMethod]
-        public void CarsSpecsBrand_Planborgini_Should_Have_Paint_With_Pink_Base()
+        public void CarsSpecsBrand_PlanfaRomeo_Should_Have_Paint_With_Blue_Base()
         {
-            //Arange, CarsSpecsBrand_Planborgini
-            var dottedPaint = new DottedPaintJob(Color.Pink, Color.Red);
-
-            //Act,
+            var painterJob = new StripedPaintJob(ParseColor(spec.BaseColor), ParseColor(spec.StripeColor));
             var painter = new Painter();
             var car = new Car(new Chassis("", true), new Engine(new EngineBlock(10), "Test"), new Interior(), new Wheel[4], Manufacturer.PlanfaRomeo, 5);
-            painter.PaintCar(car, dottedPaint);
-            var job = (DottedPaintJob)car.PaintJob;
-
-            job.DotColor.Should().Be(Color.Pink);
+            painter.PaintCar(car, painterJob);
+            var job = (StripedPaintJob)car.PaintJob;
+            job.BaseColor.Should().Be(Color.Blue);
         }
 
         [TestMethod]
-        public void CarsSpecsBrand_Planborgini_Should_Have_Paint_With_Red_Dots()
+        public void CarsSpecsBrand_PlanfaRomeo_Should_Have_Paint_With_Orange_Stripes()
         {
-          
-        }
+        var painterJob = new StripedPaintJob(ParseColor(spec.BaseColor), ParseColor(spec.StripeColor));
+        var painter = new Painter();
+        var car = new Car(new Chassis("", true), new Engine(new EngineBlock(10), "Test"), new Interior(), new Wheel[4], Manufacturer.PlanfaRomeo, 5);
+        painter.PaintCar(car, painterJob);
+        var job = (StripedPaintJob)car.PaintJob;
+        job.StripeColor.Should().Be(Color.Orange);
+         }
 
         [TestMethod]
-        public void CarsSpecsBrand_Planborgini_Should_Have_Paint_With_No_Stripe()
+        public void CarsSpecsBrand_PlanfaRomeo_Should_Have_Paint_With_No_Stripe()
         {
         }
     }
